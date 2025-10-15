c011dcb26a0c58c05c04e92ecaf47ba9d9818222
Carmelo Mayen <carmelo5mayen@gmail.com>
2025-10-14 23:00:25 -0600

    feat: useState count

---
+++ src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export const Counter = () => {
+
+    const [ count, setCount ] = useState(10);
+
+    const increaseBy = (value: number) => setCount(count + value);
+
+    return (
+        <>
+            <h2>Counter: <small>{count}</small></h2>
+            <div>
+                <button onClick={() => increaseBy(1)}>+1</button>
+                &nbsp;
+                <button onClick={() => increaseBy(-1)}>-1</button>
+            </div>
+        </>
+    )
+}
